a380deba7135205f50b6a11e8136c153133f9bb7
Connie <constanza.tubio.v@gmail.com>
2025-01-24 10:28:16 -0300

    responsive

--- js/hero-container.js
+++ js/hero-container.js
@@ -1,22 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Llamado a Hero Section
+  createHeroSection('hero-container', 'Delivery Center', 'Explora nuestros servicios');
+});
+
 function createHeroSection(containerId, title, subtitle) {
-    const container = document.getElementById(containerId);
-  
-    if (!container) {
+  const container = document.getElementById(containerId);
+
+  if (!container) {
       console.error(`Container with ID "${containerId}" no encontrado.`);
       return;
-    }
-  
-    const heroSection = document.createElement('section');
-    heroSection.classList.add('hero-section');
-  
-    heroSection.innerHTML = `
+  }
+
+  const heroSection = document.createElement('section');
+  heroSection.classList.add('hero-section');
+
+  heroSection.innerHTML = `
       <div class="overlay-deliverycenter"></div>
       <div class="text-overlay">
-        <h1>${title}</h1>
-        <h2>${subtitle}</h2>
+          <h1>${title}</h1>
+          <h2>${subtitle}</h2>
       </div>
-    `;
-  
-    container.appendChild(heroSection);
-  }
-  
+  `;
+
+  container.appendChild(heroSection);
+}
