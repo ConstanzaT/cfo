3616bc0277cd704241f1c91072a2747e0e7e4e1b
Connie <constanza.tubio.v@gmail.com>
2025-01-27 02:10:12 -0300

    outsourcing

---
+++ js/scroll_effects.js
@@ -0,0 +1,32 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+  const scrollElements = document.querySelectorAll('.scroll-animate');
+
+  const elementInView = (el, percentageScroll = 100) => {
+    const elementTop = el.getBoundingClientRect().top;
+    return (
+      elementTop <= 
+      (window.innerHeight || document.documentElement.clientHeight) * (percentageScroll / 100)
+    );
+  };
+
+  const displayScrollElement = (element) => {
+    element.classList.add('animate');
+  };
+
+  const hideScrollElement = (element) => {
+    element.classList.remove('animate');
+  };
+
+  const handleScrollAnimation = () => {
+    scrollElements.forEach((el) => {
+      if (elementInView(el, 90)) {
+        displayScrollElement(el);
+      } else {
+        hideScrollElement(el);
+      }
+    });
+  };
+
+  window.addEventListener('scroll', handleScrollAnimation);
+});
